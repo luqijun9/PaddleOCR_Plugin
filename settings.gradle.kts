fc4321d7aa217a1b52d367313b48b5beeda9a39f
Dev <dev@local.dev>
2026-08-14 09:39:54 +0000

    fix: 移除阿里云镜像(502错误), 添加aapt2.daemon=false兼容proot环境

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,16 +9,11 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
